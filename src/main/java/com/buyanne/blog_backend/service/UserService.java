70349bab3bdafa74d51282764652b82310ccf203
buyanne <1903683577@qq.com>
2023-07-14 13:26:10 +0800

    update entity

--- src/main/java/com/buyanne/blog_backend/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.buyanne.blog_backend.service;
-
-import com.buyanne.blog_backend.entity.User;
-
-public interface UserService {
-	User findUserByUsernameAndPassword(String username, String password);
-}
--- src/main/java/com/buyanne/blog_backend/service/VisitRecordService.java
+++
@@ -1,7 +0,0 @@
-package com.buyanne.blog_backend.service;
-
-import com.buyanne.blog_backend.entity.VisitRecord;
-
-public interface VisitRecordService {
-	void saveVisitRecord(VisitRecord visitRecord);
-}
