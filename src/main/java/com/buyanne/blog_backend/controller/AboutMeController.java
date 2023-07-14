32cc9937c8f2f366603169e39524a42aa35c13fd
buyanne <1903683577@qq.com>
2023-07-14 16:08:45 +0800

    关于我controller

--- src/main/java/com/buyanne/blog_backend/controller/AboutMeController.java
+++ src/main/java/com/buyanne/blog_backend/controller/AboutMeController.java
@@ -3,12 +3,16 @@ package com.buyanne.blog_backend.controller;
 import com.buyanne.blog_backend.annotation.VisitLogger;
 import com.buyanne.blog_backend.model.vo.Result;
 import com.buyanne.blog_backend.service.AboutService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class AboutMeController {
+    @Autowired
     AboutService aboutService;
 
-    
+
     @VisitLogger(behavior = "访问页面" ,content = "关于我")
     @GetMapping("/about")
     public Result about(){
