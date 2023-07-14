cc0b7d4028cd8d8192f4680d8ffde6cdf436fcbf
buyanne <1903683577@qq.com>
2023-07-14 20:24:39 +0800

    blog和site的admin权限controller

--- src/main/java/com/buyanne/blog_backend/controller/admin/SiteSettingAdminController.java
+++ src/main/java/com/buyanne/blog_backend/controller/admin/SiteSettingAdminController.java
@@ -1,33 +1,59 @@
 package com.buyanne.blog_backend.controller.admin;
 
+import com.buyanne.blog_backend.annotation.OperationLogger;
 import com.buyanne.blog_backend.entity.SiteSetting;
 import com.buyanne.blog_backend.model.vo.Result;
 import com.buyanne.blog_backend.service.SiteSettingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * @Description: 站点设置后台管理
+ * @Date: 2020-08-09
+ */
 @RestController
 @RequestMapping("/admin")
 public class SiteSettingAdminController {
+	@Autowired
+	SiteSettingService siteSettingService;
 
-    @Autowired
-    SiteSettingService siteSettingService;
+	/**
+	 * 获取所有站点配置信息
+	 *
+	 * @return
+	 */
+	@GetMapping("/siteSettings")
+	public Result siteSettings() {
+		Map<String, List<SiteSetting>> typeMap = siteSettingService.getList();
+		return Result.ok("请求成功", typeMap);
+	}
 
-    @GetMapping("/siteSettings")
-    public Result siteSettings() {
+	/**
+	 * 修改、删除(部分配置可为空，但不可删除)、添加(只能添加部分)站点配置
+	 *
+	 * @param map 包含所有站点信息更新后的数据 map => {settings=[更新后的所有配置List], deleteIds=[要删除的配置id List]}
+	 * @return
+	 */
+	@OperationLogger("更新站点配置信息")
+	@PostMapping("/siteSettings")
+	public Result updateAll(@RequestBody Map<String, Object> map) {
+		List<LinkedHashMap> siteSettings = (List<LinkedHashMap>) map.get("settings");
+		List<Integer> deleteIds = (List<Integer>) map.get("deleteIds");
+		siteSettingService.updateSiteSetting(siteSettings, deleteIds);
+		return Result.ok("更新成功");
+	}
 
-        Map<String, List<SiteSetting>> typeMap = siteSettingService.getList();
-        return Result.ok("请求成功", typeMap);
-    }
-
-    @GetMapping("/webTitleSuffix")
-    public Result getWebTieleSuffix() {
-
-        return Result.ok("请求", siteSettingService.getWebTitleSuffix());
-    }
+	/**
+	 * 查询网页标题后缀
+	 *
+	 * @return
+	 */
+	@GetMapping("/webTitleSuffix")
+	public Result getWebTitleSuffix() {
+		return Result.ok("请求成功", siteSettingService.getWebTitleSuffix());
+	}
 }
