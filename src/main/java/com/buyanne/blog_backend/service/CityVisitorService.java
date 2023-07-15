8194af251e1b61f5728df1d91dec073840b9f78c
buyanne <1903683577@qq.com>
2023-07-15 13:37:33 +0800

    delete some extra file 3

--- src/main/java/com/buyanne/blog_backend/service/CityVisitorService.java
+++
@@ -1,7 +0,0 @@
-package com.buyanne.blog_backend.service;
-
-import com.buyanne.blog_backend.entity.CityVisitor;
-
-public interface CityVisitorService {
-	void saveCityVisitor(CityVisitor cityVisitor);
-}
