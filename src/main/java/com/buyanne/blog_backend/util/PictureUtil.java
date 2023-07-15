d1948a29b18fda0006fdbebee913291711bfdcae
buyanne <1903683577@qq.com>
2023-07-15 10:56:36 +0800

    Update PictureUtil.java

--- src/main/java/com/buyanne/blog_backend/util/PictureUtil.java
+++ src/main/java/com/buyanne/blog_backend/util/PictureUtil.java
@@ -4,7 +4,7 @@ public class PictureUtil {
 
 	public static String randomBlogFirstPicture() {
 		int m = 0;
-		int n = 9;
+		int n = 14;
 		int temp=m+(int)(Math.random()*(n+1-m));
 		return "https://cdn.jsdelivr.net/gh/buyanne/JsDelivr/ContentThumb/"+temp+".jpg";
 	}
